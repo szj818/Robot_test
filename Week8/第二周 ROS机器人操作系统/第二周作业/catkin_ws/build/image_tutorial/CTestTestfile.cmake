835a984947f65ebcf12d53e5a92f3991974eb08a
zj Sun <zjsun@Space-2.local>
2026-04-26 22:19:38 +0800

    add some files

--- Week8/第二周 ROS机器人操作系统/第二周作业/catkin_ws/build/image_tutorial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sunzj/Robot/catkin_ws/src/image_tutorial
-# Build directory: /home/sunzj/Robot/catkin_ws/build/image_tutorial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
